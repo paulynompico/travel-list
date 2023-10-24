4649b57e789167ac0427a590f95ee7382b7fe9ad
Paulyn Ompico <ompicopaulyn@gmail.com>
2023-10-24 20:05:27 +0200

    Render test items on list

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import Header from "./Header";
 import Form from "./Footer";
-import List from "./List";
+// import List from "./List";
 import Footer from "./Footer";
+
+const testItems = [
+  { id: 1, description: "Passports", quantity: 2 },
+  { id: 2, description: "Residence card", quantity: 2 },
+  { id: 3, description: "Wallet", quantity: 2 },
+  { id: 4, description: "Hedi's underwear", quantity: 3 },
+];
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -12,11 +19,27 @@ export default function App() {
   }
 
   return (
-    <div className="container">
+    <div className="app">
       <Header />
       <Form onAddItems={handleAddItems} />
       <List />
       <Footer />
     </div>
   );
+}
+
+function List() {
+  return (
+    <div className="list">
+      <ul>
+        {testItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
 }
